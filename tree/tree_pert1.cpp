24bacf263d804194194e009d214649af9c31a2ae
IJU666 <fauzimaulanabandung2020@gmail.com>
2024-06-24 08:39:30 +0700

    add other function

--- tree/tree_pert1.cpp
+++ tree/tree_pert1.cpp
@@ -84,6 +84,231 @@ bool cekIsiTree(){
     }   
 }
 
+void update(char newLabel, tree *node){
+    if (!root)
+    {
+        cout << "Buat tree terlebih dahulu" << endl;
+    } else
+    {
+        if (!node)
+        {
+            cout << "Yang ingin dicari tidak ada" << endl;
+        } else
+        {
+            char temp = node -> label;
+            node -> label = newLabel;
+            cout << "Label node " << temp << " Berhasil di ubah menjadi" << newLabel << endl;
+        } 
+    }
+}
+
+void retrieve(tree *node){
+    cout << "Fungsi retrieve" << endl;
+    if (!root)
+    {
+        cout << "\n Buat tree terlebih dahulu" << endl;
+    } else
+    {
+        if (!node)
+        {
+            cout << "\n Node yang ditunjuk tidak ada" << endl;
+        } else
+        {
+            cout << "Label Node : " << node -> label << endl;
+        }
+    }
+}
+
+void find(tree *node){
+    if (!root)
+    {
+        cout << "\n Buat data tree terlebih dahulu" << endl;
+    } else
+    {
+        if (!node)
+        {
+            cout << "\n Node yang ditunjuk tidak ada" << endl;
+        } else
+        {
+            cout << "\nLabel Node : " << node -> label << endl;
+            cout << "\nRoot Node : " << root -> label << endl;
+
+            // cek parent
+            if (node -> parent == NULL)
+            {
+                cout << "Tidak punya orang tua" << endl;
+            } else
+            {
+                cout << "Parent Node : " << node -> parent -> label << endl;
+            }  
+
+            // cek sibling
+            if (node -> parent != NULL && node -> parent -> left != node && node -> parent -> right == node)
+            {
+                cout << "\n Saudara kiri : " << node -> parent -> left -> label << endl;
+            } else if (node -> parent != NULL && node -> parent -> right != node && node -> parent -> left == node)
+            {
+                cout << "\n Saudara Kanan : " << node -> parent -> right -> label << endl;
+            } else
+            {
+                cout << "Tidak ada sodara" << endl;
+            }
+            
+            //cek anak kiri
+            if (node -> left == NULL)
+            {
+                cout << "Tidak mempunyai anak kiri" << endl;
+            } else
+            {
+                cout << "Anak kiri node : " << node -> right -> label << endl;
+            }
+        } 
+    }  
+}
+
+void preOrder(tree *node = root){
+    if (root == NULL)
+    {
+        cout << "\n Buat tree terleboih dahulu" << endl;
+    } else
+    {
+        if (node != NULL)
+        {
+            cout << node -> label << ",";
+            preOrder(node-> left);
+            preOrder(node-> right);
+        }   
+    }
+}
+
+void inOrder(tree *node = root){
+    if (root == NULL)
+    {
+        cout << "\n Buat tree terleboih dahulu" << endl;
+    } else
+    {
+        if (node != NULL)
+        {
+            inOrder(node-> left);
+            cout << node -> label << ", ";
+            inOrder(node-> right);
+        }   
+    }
+}
+
+void postOrder(tree *node = root){
+    if (root == NULL)
+    {
+        cout << "\n Buat tree terleboih dahulu" << endl;
+    } else
+    {
+        if (node != NULL)
+        {
+            postOrder(node-> left);
+            postOrder(node-> right);
+            cout << node -> label << ", ";
+        }   
+    }
+}
+
+void deleteTree(tree *node){
+    if (root == NULL)
+    {
+        cout << "\n Buat tre terlebih dahulu" << endl;
+    } else
+    {
+        if (node != NULL)
+        {
+            if (node != root)
+            {
+                node -> parent -> left = NULL;
+                node -> parent -> right = NULL;
+            }
+            deleteTree(node->left);
+            deleteTree(node->right);
+        }
+        
+        if (node == root)
+        {
+            delete root;
+            root == NULL;
+        } else
+        {
+            delete node;
+        }
+    }
+}
+
+void deleteSub(tree *node){
+    if (root == NULL)
+    {
+        cout << "\n Buat tree terlebih dahulu" << endl;
+    } else
+    {
+        deleteTree(node -> left);
+        deleteTree(node -> right);
+        cout << "Subtree dari node " << node -> label << " berhasil dihapus" << endl;
+    }  
+}
+
+void clear(){
+    if (root == NULL)
+    {
+        cout << "\nBuat Tree terlebih dahulu" << endl;
+    } else
+    {
+        deleteTree(root);
+        cout << "\nTree berhasil dihapus" << endl;
+    }
+}
+
+int size(tree *node = root){
+    if (!root)
+    {
+        cout << "\nBuat tree terlebih dahulu" << endl;
+    } else
+    {
+        if (!node)
+        {
+            return 0;
+        } else
+        {
+            return 1 + size(node -> left) + size(node -> right);
+        }
+    }
+}
+
+int height(tree *node = root){
+    if (!root)
+    {
+        cout << "\nBuat tree terlebih dahulu" << endl;
+    } else
+    {
+        if (!node)
+        {
+            return 0;
+        } else
+        {
+            int heightKiri = height(node -> left);
+            int heightKanan = height(node -> right);
+            // bandingkan nilai heightkanan dan kiri
+            if (heightKiri >= heightKanan)
+            {
+                return heightKiri + 1;
+            } else
+            {
+                return heightKanan + 1;
+            }  
+        }
+    }   
+}
+
+void karakteristik(){
+    cout << "\nSize of tree : " << size() << endl;
+    cout << "Height : " << height() << endl;
+    cout << "Average of tree" << size()/height() << endl;
+}
+
 int main (){
     createDataAwalRoot('A');
     tree *nodeB, *nodeC, *nodeD, *nodeE, *nodeF, *nodeG, *nodeH, *nodeI, *nodeJ;
